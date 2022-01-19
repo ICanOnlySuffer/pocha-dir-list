d4d9454479e333dbc446e3a0238a80a9b495a2f7
ICanOnlySuffer <pierorojas@tutamail.com>
2022-01-19 01:55:05 -0500

    added version.h

---
+++ src/ptree/flags/version.h
@@ -0,0 +1,20 @@
+# ifndef _FLAG_VERSION_
+# define _FLAG_VERSION_
+# define VERSION "1.0.4"
+
+nil version () {
+	puts ("ptree v" VERSION " - ICanOnlySuffer (Piero Rojas)");
+	puts (LANG ("credits"));
+	puts (
+		"  Steve Baker\n"     \
+		"  Thomas Moore\n"    \
+		"  Francesc Rocher\n" \
+		"  Florian Sesser\n"  \
+		"  Kyosuke Tokoro"
+	);
+	puts (LANG ("copyright"));
+}
+
+# endif // _FLAG_VERSION_
+
+
