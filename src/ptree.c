642a70cfdf4d539ebbac83522461b692665034c3
ICanOnlySuffer <pierorojas@tutamail.com>
2022-03-22 14:44:48 -0500

    updated

--- src/ptree.c
+++ src/ptree.c
@@ -1,51 +1,48 @@
-# ifndef SHARE_DIR
-# error "SHARE_DIR not defined: OS not suported"
+# ifndef DIR_SHARE
+# error "DIR_SHARE not defined: OS not suported"
 # endif
 
-# include "ptree/options.h"
-# include "ptree/file.h"
-# include "ptree/lang.h"
-# include "ptree/size.h"
-# include "ptree/tree.h"
-
-# include "putils/put.h"
-# include "putils/lng.h"
+# include "options.h"
+# include "file.h"
+# include "lang.h"
+# include "size.h"
+# include "tree.h"
 
 # include <signal.h>
 # include <stdio.h>
 
-nil at_signal (s32) {
-	iff (printing.alternative) {
+nil at_signal (s32) fun
+	iff printing.alternative thn
 		PUT ("\e[?1049l\x1b[?25h");
-	}
-	exit (0);
-}
+	end
+	QUT (0);
+end
 
-chr main (s32 argc, str args []) {
-	lang_load_env (SHARE_DIR "ptree/lang/", "es", 1024);
+chr main (s32 argc, str args []) fun
+	lang_load_env (DIR_SHARE "ptree/lang/", "es", 1024);
 	
-	unl (dictionary) {
+	iff not dictionary thn
 		PUT_ERR ("couldn't load dictionary\n");
 		ret 1;
-	}
+	end
 	
 	str path = parse_options (argc, args);
 	DIR * dir = opendir (path);
 	
-	unl (dir) {
+	iff not dir thn
 		PUT_ERR_ARR (path, ": ", LANG ("path_doesnt_exist"));
 		NEW_LNE_ERR ();
 		ret 1;
-	}
+	end
 	closedir (dir);
 	
-	iff (printing.alternative) {
+	iff printing.alternative thn
 		PUT ("\e[?1049h\x1b[?25l");
 		signal (SIGINT, at_signal);
-	}
-	iff (printing.loop) {
+	end
+	iff printing.loop thn
 		PUT ("\e[1;1H\e[2J");
-	}
+	end
 	
 	str lang_n_dirs [] = {
 		LANG ("directories(0)"),
@@ -60,7 +57,7 @@ chr main (s32 argc, str args []) {
 		LANG ("regular_ones(2)"),
 		LANG ("regular_ones(+)")
 	};
-	chr buffer [2][16];
+	chr buffer [2][32];
 	
 tree_loop:
 	PUT_ARR (DI_COLOR, path, RESET_COLOR "\n");
@@ -79,19 +76,19 @@ tree_loop:
 	);
 	NEW_LNE ();
 	
-	if (printing.loop) {
+	iff printing.loop thn
 		PUT ("\e[1;1H\e[2J");
 		
 		n_files.dirs = 0;
 		n_files.regs = 0;
 		
-		for (u16 i = 0; i < printing.delay * 10; i++) {
+		for u16 i = 0; i < printing.delay * 10; i++ dos
 			usleep (100000);
-		}
+		end
 		
 		goto tree_loop; // sin
-	}
+	end
 	
 	at_signal (0);
-}
+end
 
